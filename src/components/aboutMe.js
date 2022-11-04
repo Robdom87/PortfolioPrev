6c4cd2b44d5eba11394817e891d214705e221df0
Cedar Chavez <geggas00@gmail.com>
2022-11-03 17:15:22 -0700

    add updated resume

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,8 +7,8 @@ function AboutMe() {
         <section className="aboutMe">
             <h2>"If it illicits fear, it must be done." - Anonymous</h2>
             <p>
-                I am a recent grad whose itch to learn new things pushed into an entirely new career trajectory, software development.
-                I am always excited to take on a new challenge and coming from a accounting heavy background, I am not afraid to tackle big data sets.  
+                I am a recent grad whose itch to learn new things pushed him into an entirely new career trajectory, software development.
+                I am always excited to take on a new challenge and coming from an accounting heavy background, I am not afraid to tackle big data sets.  
             </p>
             <a href={resume} download="resume" target="_blank">
                 <Button variant="outline-secondary">Download Resume</Button>
